8ae8a09cd2c8908c116e53d9b818a742460a4532
diegogalli <84423207+diegogalli@users.noreply.github.com>
2021-08-11 13:55:31 -0300

    Uri 1005 Media

---
+++ Media1/Media1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace Media1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double A, B, MEDIA;
+
+            A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            MEDIA = ((A * 3.5) + (B * 7.5)) / 11; // nota A com peso 3.5 e nota b com peso 7.5
+
+            Console.WriteLine("Media = " + MEDIA.ToString("F5", CultureInfo.InvariantCulture));
+        }
+    }
+}
